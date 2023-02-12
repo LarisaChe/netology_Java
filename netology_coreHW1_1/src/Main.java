c0981163a093d803907ea3f8b0f591384018bbce
Larisa Che <mavros@mail.ru>
2023-02-12 21:36:17 +0300

    lambda/task1/Калькулятор

---
+++ netology_coreHW1_1/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        calc.println.accept(a);
+        //int b = calc.minus.apply(1,1);
+        //int c = calc.devide.apply(a, b);  - будет ошибка, так как b = 0, а на ноль делить нельзя
+
+        int b = calc.minus.apply(1,1);
+        calc.println.accept(b);
+
+        int d = calc.multiply.apply(1,1);
+        calc.println.accept(d);
+
+        int c = calc.devide.apply(a, d);
+        calc.println.accept(c);
+
+        int e = calc.pow.apply(3);
+        calc.println.accept(e);
+
+        int f = calc.abs.apply(-5);
+        calc.println.accept(f);
+
+        boolean g = calc.isPositive.test(3);
+        System.out.println(g);
+    }
+}
