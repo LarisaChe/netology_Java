3383df4aa2b976f79d849020dbdff103f60de810
Larisa Che <mavros@mail.ru>
2023-02-14 21:22:07 +0300

    lambda/task1/Калькулятор Доработка

--- netology_coreHW1_1/src/Main.java
+++ netology_coreHW1_1/src/Main.java
@@ -5,17 +5,17 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         calc.println.accept(a);
-        //int b = calc.minus.apply(1,1);
-        //int c = calc.devide.apply(a, b);  - будет ошибка, так как b = 0, а на ноль делить нельзя
-
         int b = calc.minus.apply(1,1);
-        calc.println.accept(b);
+        int c = calc.devide.apply(a, b);  //- будет ошибка, так как b = 0, а на ноль делить нельзя
+
+        int b1 = calc.minus.apply(1,1);
+        calc.println.accept(b1);
 
         int d = calc.multiply.apply(1,1);
         calc.println.accept(d);
 
-        int c = calc.devide.apply(a, d);
-        calc.println.accept(c);
+        int c1 = calc.devide.apply(a, d);
+        calc.println.accept(c1);
 
         int e = calc.pow.apply(3);
         calc.println.accept(e);
