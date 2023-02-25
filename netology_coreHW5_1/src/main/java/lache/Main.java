ad55fd4b3abd6fee7b5befac75449e39f6e4f326
LaChe <chernaya_larisa@mail.ru>
2023-02-25 14:28:11 +0300

    5.3. JSON парсер
    special_files/task3/
    Задача 3: JSON парсер

--- netology_coreHW5_1/src/main/java/lache/Main.java
+++ netology_coreHW5_1/src/main/java/lache/Main.java
@@ -7,11 +7,13 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-//import org.w3c.dom.Element;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-//import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -21,27 +23,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-
-//import static jdk.jfr.internal.jfc.JFCParser.parseXML;
 
 public class Main {
-    private static List<Employee> employeesFromCSV = new ArrayList<Employee>();
-    private static List<Employee> employeesFromXML = new ArrayList<Employee>();
+    private static List<Employee> employeesFromCSV = new ArrayList<>();
+    private static List<Employee> employeesFromXML = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileNameCSV = "data.csv";
         String fileNameXML = "data.xml";
+        String fileNameJson = "new_data.json";
 
+        // Задача 1: CSV - JSON парсер
         parseCSV(columnMapping, fileNameCSV);
         String json1 = listToJson(employeesFromCSV);
         saveJson(json1, "data.json");
 
+        // Задача 2: XML - JSON парсер
         parseXML(fileNameXML);
-        System.out.println(employeesFromXML.toString());
+        //System.out.println(employeesFromXML.toString());
         String json2 = listToJson(employeesFromXML);
         saveJson(json2, "data2.json");
+
+        // Задача 3: JSON парсер
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(fileNameJson));
+            JSONArray jsonArray = (JSONArray) obj;
+            List<Employee> list = jsonToList(jsonArray);
+            //System.out.println(list.toString());
+        } catch (IOException | ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static List<Employee> jsonToList(JSONArray jsonArray) {
+        List<Employee> list = new ArrayList<>();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        for (Object json3 : jsonArray) {
+            Employee employee = gson.fromJson(json3.toString(), Employee.class);
+            list.add(employee);
+            System.out.println(employee.toString());
+        }
+        return list;
     }
     private static void parseXML(String fileNameXML) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -70,8 +95,7 @@ public class Main {
                     if (Node.ELEMENT_NODE == node2.getNodeType()) {
                         switch (node2.getNodeName()) {
                             case "id":
-                                //System.out.println(Long.valueOf(node2.getTextContent()).longValue());
-                                id = Long.valueOf(node2.getTextContent()).longValue();
+                                id = Long.valueOf(node2.getTextContent());
                                 break;
                             case "firstName":
                                 firstName = node2.getTextContent();
@@ -83,7 +107,6 @@ public class Main {
                                 country = node2.getTextContent();
                                 break;
                             case "age":
-                                //System.out.println(Integer.valueOf(node2.getTextContent()));
                                 age = Integer.valueOf(node2.getTextContent());
                                 break;
                         }
@@ -105,7 +128,7 @@ public class Main {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         StringBuilder json = new StringBuilder();
-        json = json.append(gson.toJson(employeeList, new TypeToken<List<Employee>>() {}.getType()));
+        json.append(gson.toJson(employeeList, new TypeToken<List<Employee>>() {}.getType()));
         return json.toString();
     }
 
