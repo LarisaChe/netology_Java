43a8a66381c6a7d94a09b6edf274c0590c54f294
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-03-03 08:33:28 +0300

    /junit/task1/ исправление ошибок
    Использование JUnit

--- netology_coreHW6_1/src/test/java/MainTest.java
+++ netology_coreHW6_1/src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import javacore36.lache.Main;
 import javacore36.lache.User;
 import javacore36.lache.exception.AccessDeniedException;
 import javacore36.lache.exception.UserNotFoundException;
@@ -9,7 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static javacore36.lache.Main.getUserByLoginAndPassword;
 import static javacore36.lache.Main.validateUser;
 
-public class Main {
+public class MainTest {
 
     @ParameterizedTest
     @CsvSource({
@@ -17,22 +18,17 @@ public class Main {
             "Jack, pass2",
             "Fil, pass4"
     })
-    public void testGetUserByLoginAndPasswordWithParams(String login, String password) throws UserNotFoundException {
-        User testUser = getUserByLoginAndPassword(login, password);
-
-//        Assertions.assertDoesNotThrow();
-        System.out.println("Тест testGetUserByLoginAndPasswordWithParams завершен");
+    public void testGetUserByLoginAndPasswordWithParams(String login, String password)  {
+        Assertions.assertDoesNotThrow(() -> Main.getUserByLoginAndPassword(login, password));
     }
 
     @Test
     public void testGetUserByLoginAndPassword() throws UserNotFoundException {
         User expectedUser = new User("Jhon", "jhon@gmail.com", "pass1", 24);
 
-        //User testUser = getUserByLoginAndPassword("Jhon", "pass1");
         User testUser = getUserByLoginAndPassword(expectedUser.getLogin(), expectedUser.getPassword());
 
         Assertions.assertEquals(expectedUser, testUser);
-        System.out.println("Тест testGetUserByLoginAndPassword завершен");
     }
 
     @Test
@@ -40,14 +36,12 @@ public class Main {
         Assertions.assertThrows(UserNotFoundException.class,() ->{
             getUserByLoginAndPassword("Mary", "Ann");
                     });
-        System.out.println("Тест testGetUserByLoginAndPassword_throwsException завершен");
     }
 
     @Test
     public void testValidateUser() throws AccessDeniedException, UserNotFoundException {
         User testUser = new User("Jhon", "jhon@gmail.com", "pass1", 24);
         Assertions.assertTrue(validateUser(testUser));
-        System.out.println("Тест testValidateUser завершен");
     }
 
     @Test
@@ -55,6 +49,5 @@ public class Main {
         User testUser = new User("Jack", "jack@gmail.com", "pass2", 17);
         Assertions.assertThrows(AccessDeniedException.class,() ->{ validateUser(testUser);
         });
-        System.out.println("Тест testValidateUser_throwsException завершен");
     }
 }
