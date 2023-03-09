c7bb2821a87a34bffb20f00fc3c07a966a6234c1
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-03-09 12:45:53 +0300

    network/task2/с рюшечками
    network/task2/
    Задача "Клиент-сервер с рюшечками"

--- netology_coreHW8_1/src/main/java/ru/netology/lache/MainClient.java
+++ netology_coreHW8_1/src/main/java/ru/netology/lache/MainClient.java
@@ -5,18 +5,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainClient {
     public static void main(String[] args) {
-        String host = "localhost";
-        int port = 8090;
+        String host = "netology.homework";
+        int port = 8095;
+        List<String> listStr = new ArrayList<>();
+        listStr.add("А у меня в кармане гвоздь! А у вас?");
+        listStr.add("А у нас сегодня кошка родила вчера котят. Котята выросли немножко, но есть из блюдца не хотят!");
+        listStr.add("А у нас водопровод! Вот!");
+
+        System.out.println("CLIENT");
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            //out.println("GET / HTTP1.1 \n" + "Host: local \n\n\n");
-            out.println("Larisa");
-            String resp = in.readLine();
-            System.out.println(resp);
+
+            String resp = "";
+            int i = 0;
+            while ((resp = in.readLine()) != null) {
+                System.out.println(resp);
+
+                System.out.println("     " + listStr.get(i));
+                out.println(listStr.get(i));
+                i++;
+            }
         } catch (Exception e) {
             e.getStackTrace();
         }
