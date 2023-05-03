e9382a5969ec4a2535bfa8b5efb8bbba28e4ea8a
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-05-03 16:22:05 +0300

    Stub and tests for add
    tdd_ddd/task1/ Задача 1. Разработка через тестирование
    Телефонная книга
    Stub and tests for add

--- netology_jadv36_5/src/main/java/lache/PhoneBook.java
+++ netology_jadv36_5/src/main/java/lache/PhoneBook.java
@@ -1,4 +1,7 @@
 package lache;
 
 public class PhoneBook {
+    public static int add(String name, String phone) {
+        return 0;
+    }
 }
--- netology_jadv36_5/src/test/java/lache/PhoneBookTest.java
+++ netology_jadv36_5/src/test/java/lache/PhoneBookTest.java
@@ -1,4 +1,35 @@
 package lache;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class PhoneBookTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, Jhon, +79011012233",
+            "2, Jack, 89052024455",
+            "3, Fil, +79153036677"
+    })
+    public void testAddSimple(int resultNum, String name, String phone) {
+
+        Assertions.assertEquals(resultNum, PhoneBook.add(name, phone));
+    }
+
+    @Test
+    public void testAdd_DoubleName() {
+        PhoneBook.add("Jhon", "+79011012233");
+        Assertions.assertEquals(1, PhoneBook.add("Jhon", "+79011012233"));
+    }
+
+    @Test
+    public void testAdd_nullName_throwsException() {
+        Assertions.assertThrows(Exception.class, () -> {
+            int i = PhoneBook.add(null, "+7777");
+        });
+    }
+
+
 }
