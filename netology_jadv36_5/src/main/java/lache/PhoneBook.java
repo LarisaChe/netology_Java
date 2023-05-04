fdefc34d74074e4dbd71210f7a00a4b43e23b582
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-05-04 11:54:27 +0300

    Stub and tests for findByNumber
    tdd_ddd/task1/
    Задача 1. Разработка через тестирование
    Телефонная книга
    Stub and tests for findByNumber

--- netology_jadv36_5/src/main/java/lache/PhoneBook.java
+++ netology_jadv36_5/src/main/java/lache/PhoneBook.java
@@ -5,10 +5,20 @@ import java.util.TreeMap;
 
 public class PhoneBook {
     private static Map<String, String> phoneBook = new TreeMap<>();
-    public static int add(String name, String phone) {
+    public static int add(String name, String number) {
         if (!phoneBook.containsKey(name)) {
-            phoneBook.put(name, phone);
+            phoneBook.put(name, number);
         }
         return phoneBook.size();
     }
+
+    public static String findByNumber(String number) {
+        return null;
+    }
+
+    public static int size() {
+        return phoneBook.size();
+    }
+
+
 }
--- netology_jadv36_5/src/test/java/lache/PhoneBookTest.java
+++ netology_jadv36_5/src/test/java/lache/PhoneBookTest.java
@@ -1,20 +1,25 @@
 package lache;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class PhoneBookTest {
 
+    @BeforeAll
+    public static void testAdd_Record() {
+        Assertions.assertEquals(1, PhoneBook.add("Helen", "+79256065544"));
+    }
     @ParameterizedTest
     @CsvSource({
-            "1, Jhon, +79011012233",
-            "2, Jack, 89052024455",
-            "3, Fil, +79153036677"
+            "Jhon, +79011012233",
+            "Jack, 89052024455",
+            "Fil, +79153036677"
     })
-    public void testAddSimple(int resultNum, String name, String phone) {
-
+    public void testAdd_SomeRecords(String name, String phone) {
+        int resultNum = PhoneBook.size() + 1;
         Assertions.assertEquals(resultNum, PhoneBook.add(name, phone));
     }
 
@@ -26,10 +31,28 @@ public class PhoneBookTest {
 
     @Test
     public void testAdd_nullName_throwsException() {
-        Assertions.assertThrows(Exception.class, () -> {
+        Assertions.assertThrows(NullPointerException.class, () -> {
             int i = PhoneBook.add(null, "+7777");
         });
     }
 
+    @Test
+    public void testFindByNumber_Found() {
+        String name = "Helen";
+        String phone = "+79256065544";
+        Assertions.assertEquals(name, PhoneBook.findByNumber(phone));
+    }
 
+    @Test
+    public void testFindByNumber_NotFound() {
+        String name = "Number 81112223344 isn't in PhoneBook";
+        String phone = "81112223344";
+        Assertions.assertEquals(name, PhoneBook.findByNumber(phone));
+    }
+    @Test
+    public void testFindByNumber_throwsException() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            String s = PhoneBook.findByNumber(null);
+        });
+    }
 }
