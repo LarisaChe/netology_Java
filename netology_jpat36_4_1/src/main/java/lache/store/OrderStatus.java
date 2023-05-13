21e322e21726dc0ded8ef7378c18f65c0f40ebb2
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-05-13 17:13:20 +0300

    solid/task1/ Задача Магазин Product
    изменения Product
    solid/task1/ Задача Магазин

--- netology_jpat36_4_1/src/main/java/lache/store/OrderStatus.java
+++ netology_jpat36_4_1/src/main/java/lache/store/OrderStatus.java
@@ -1,10 +1,44 @@
 package lache.store;
 
 public enum OrderStatus {
-    CREATED,
-    GATHERING,
-    READY_FOR_DELIVERY,
-    DELIVERY,
-    RECEIVED,
-    COMPLETED
+    CANCELLED ("отменен"),
+    CREATED ("создан"),
+    GATHERING ("собирается"),
+    READY_FOR_SHIPMENT ("готов к доставку"),
+    ON_THE_WAY ("доставляется"),
+    RECEIVED ("вручен получателю"),
+    COMPLETED ("завершен");
+
+    private String descRu;
+    OrderStatus(String descRu){
+        this.descRu = descRu;
+    }
+    public String getDescription(){ return descRu;}
+    public OrderStatus next(OrderStatus currentStatus) {
+        switch (currentStatus) {
+            case CREATED:
+                return GATHERING;
+            case GATHERING:
+                return READY_FOR_SHIPMENT;
+            case READY_FOR_SHIPMENT:
+                return ON_THE_WAY;
+            case RECEIVED:
+                return COMPLETED;
+            default:
+                System.out.println("Невозможно изменение статуса " + currentStatus);
+                return currentStatus;
+        }
+    }
+
+    public OrderStatus cancel(OrderStatus currentStatus) {
+        switch (currentStatus) {
+            case CREATED:
+            case GATHERING:
+            case READY_FOR_SHIPMENT:
+                return CANCELLED;
+            default:
+                System.out.println("Невозможно отменить заказ из статуса " + currentStatus);
+                return currentStatus;
+        }
+    }
 }
--- netology_jpat36_4_1/src/main/java/lache/store/Product.java
+++ netology_jpat36_4_1/src/main/java/lache/store/Product.java
@@ -3,21 +3,13 @@ package lache.store;
 public class Product {
     String key;
     String name;
-    String madeBy;
-    double weight;
-    String measure;
-    String tags;
     double price;
     int num;
 
     @Override
     public String toString() {
         return key + " / " +
-                 name +  " / " +
-                madeBy +  " / " +
-                weight +
-                measure +  " / "  +
-                tags +  " / "  +
+                name +  " / " +
                 price + " / " +
                 num;
     }
